0a640fc205d103ff805e5c5e89bb581c1239831c
NadeNoob <hampusviskan12@gmail.com>
2025-07-15 13:04:12 +0200

    Moved the "IsJson" function

---
+++ src/client/modules/isJson.ts
@@ -0,0 +1,11 @@
+export default function IsJson(text: string) {
+  return /^[\],:{}\s]*$/.test(
+    text
+      .replace(/\\["\\\/bfnrtu]/g, "@")
+      .replace(
+        /"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,
+        "]"
+      )
+      .replace(/(?:^|:|,)(?:\s*\[)+/g, "")
+  );
+}
